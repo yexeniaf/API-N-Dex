1cf3b9b77d066cda74f112641649b87155f577dd
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 14:39:00 -0500

    paths and elements

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ function App() {
       <nav>THIS WILL BE A NAVBAR</nav>
       <Routes>
         <Route path="/" element={<div>API-N-Dex</div>} />
-        <Route />
-        <Route />
-        <Route />
+        <Route path="/apis" element={<div>API List</div>} />
+        <Route path="/apis/:id" element={<div>API Details</div>}/>
+        <Route path="/new" element={<div>New API</div>}/>
       </Routes>
 
     </div>
