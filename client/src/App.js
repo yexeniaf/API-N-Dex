6666efe5186152b938cf84c81ac7829cc2d8de39
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 14:18:02 -0500

    installation of routes and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
  
   return (
     <div className="App">
       <nav>THIS WILL BE A NAVBAR</nav>
-      
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+
     </div>
   );
 }
