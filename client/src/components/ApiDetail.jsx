64ebb28368222887c1c9c84cbc83a601e8a3c71e
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 17:45:45 -0500

    rename of api and added link field

--- client/src/components/ApiDetail.jsx
+++ client/src/components/ApiDetail.jsx
@@ -1,28 +1,33 @@
 import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import api from '../services/apiConfig';
 import DeleteButton from './DeleteButton';
 
+
 export default function ApiDetail() {
-    const [ api, setApi] = useState({});
+    const [ apiDetail, setApiDetail] = useState({});
     const {id} = useParams();
 
     useEffect(() => {
         const fetchApi = async () => {
             const res = await api.get(`/${id}`)
-            setApi(res.data)
+            setApiDetail(res.data)
         }
         fetchApi();
     }, [])
 
-    if (!api.fields) {
+    if (!apiDetail.fields) {
         return <div>LOADING</div>
     }
 
     return (
         <div>
-            <h2>{api.fields?.title}</h2>
-            <img style={{width: "300px"}} src={api.fields.image} alt={api.fields.title}/>
-            <h3>{api.fields?.description}</h3> 
+            <h2>{apiDetail.fields?.title}</h2>
+            <img style={{width: "300px"}} src={apiDetail.fields.image} alt={apiDetail.fields.title}/>
+            <h3>{apiDetail.fields?.description}</h3> 
+            <br/>
+            <h4>{apiDetail.fields?.link}</h4>
+            <br/>
             <DeleteButton id={id}/>
         </div>
     )
