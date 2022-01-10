7ca69a8b589780d8fdacdc55631dac12cd6fcc28
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 15:04:33 -0500

    navbar component homepage link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
     return (
         <div>
-            
+            <Link to="/">
+                <h1>API-N-Dex</h1>
+            </Link>
         </div>
     )
 }
