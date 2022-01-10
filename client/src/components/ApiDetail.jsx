81029e2260c65980d69ae6f3d8407cc146158b37
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 17:29:44 -0500

    delete button in detail jsx

--- client/src/components/ApiDetail.jsx
+++ client/src/components/ApiDetail.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom"
+import DeleteButton from './DeleteButton';
 
 export default function ApiDetail() {
     const [ api, setApi] = useState({});
@@ -18,7 +19,7 @@ export default function ApiDetail() {
             <h2>{api.fields.title}</h2>
             <img style={{width: "300px"}} src={api.fields.image} alt={api.fields.title}/>
             <h3>{api.fields.description}</h3> 
-            <button>Delete API</button>
+            <DeleteButton id={id}/>
         </div>
     )
 }
