6c6ac0070743591fb3098e96c277e03a4cd546c2
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 17:21:15 -0500

    const navigate and handle delete

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import api from "../services/apiConfig";
+import { useNavigate } from "react-router-dom";
 
-export default function DeleteButton() {
+
+export default function DeleteButton(props) {
+    const navigate = useNavigate();
+
+    const handleDelete = async () => {
+        const res = await api.delete(`/${props.id}`)
+        navigate("/apis")
+    }
+
     return (
         <div>
             
