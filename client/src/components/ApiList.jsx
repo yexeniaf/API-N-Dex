88e11027d87d0a2de4d157cb730a140fb62e04fa
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 15:30:32 -0500

    use effect function

--- client/src/components/ApiList.jsx
+++ client/src/components/ApiList.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import api from "../services/apiConfig";
 
 export default function ApiList() {
     return (
