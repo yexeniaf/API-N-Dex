167cd4ac4055cd79553c655e84f61f298fc71a6a
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 16:45:10 -0500

    use effect in api detail jsx

--- client/src/components/ApiDetail.jsx
+++ client/src/components/ApiDetail.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom"
 
 export default function ApiDetail() {
     const [ api, setApi] = useState({});
     const {id} = useParams();
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const res = await api.get(`/${id}`)
+            setApi(res.data)
+        }
+        fetchApi();
+    }, [])
 
     return (
         <div>
