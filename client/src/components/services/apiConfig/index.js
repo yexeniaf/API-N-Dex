2488acec6d3a90cf2a2f4e1acf87c67264458588
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 14:03:12 -0500

    baseURL and api in index js

--- client/src/components/services/apiConfig/index.js
+++ client/src/components/services/apiConfig/index.js
@@ -2,3 +2,14 @@ import axios from "axios";
 
 const BASE = process.env.REACT_APP_AIRTABLE_BASE;
 const KEY = process.env.REACT_APP_AIRTABKE_KEY;
+
+const baseURL = `https://api.airtable.com/v0/${BASE}/Characters`;
+
+const api = axios.create({
+    baseURL,
+    headers: {
+        Authorization: `Bearer ${KEY}`,
+    }
+})
+
+export default api;
