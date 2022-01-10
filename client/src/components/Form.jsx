0233b527ef3ee035122892b11fd1096463f4b0d9
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 18:05:58 -0500

    input of description, link, and image url

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -9,6 +9,24 @@ export default function Form(props) {
                 name="title"
                 value={title}
                 placeholder="Add API Title"
+            />   
+            <label>Description</label>
+            <input 
+                name="description"
+                value={description}
+                placeholder="Add API Description"
+            />   
+            <label>Link</label>
+            <input 
+                name="link"
+                value={link}
+                placeholder="Add API Link"
+            />   
+            <label>API Image Url</label>
+            <input 
+                name="imageUrl"
+                value={imageUrl}
+                placeholder="Add the API's image URL"
             />     
         </form>
     )
