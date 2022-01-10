a8cc23e8b245a58f724d3c78f0891cb2933558a6
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 14:48:36 -0500

    rfc default for components

---
+++ client/src/components/ApiList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ApiList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ client/src/components/NewApi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewApi() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
