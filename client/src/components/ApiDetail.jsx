460fd9a1f390c1e21a365e9ea5724cd101f7eef2
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 16:40:56 -0500

    use params in api detail jsx

--- client/src/components/ApiDetail.jsx
+++ client/src/components/ApiDetail.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-
+import { useParams } from "react-router-dom"
 
 export default function ApiDetail() {
     const [ api, setApi] = useState({});
+    const {id} = useParams();
 
     return (
         <div>
