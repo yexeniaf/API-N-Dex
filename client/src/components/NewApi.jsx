c8c7964fb2cec9d478e1109dea97691127d0aed2
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-10 19:32:56 -0500

    input cost in new api jsx

--- client/src/components/NewApi.jsx
+++ client/src/components/NewApi.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Form from "./Form";
 import api from "../services/apiConfig";
+
+const default_input = {
+    title: "",
+    imageUrl: "",
+    description: "",
+    link: "",
+}
 
 export default function NewApi() {
     return (
