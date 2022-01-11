06782ff751425fff3ef14db6f373612ee9dbd3e9
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-11 16:15:46 -0500

    renaming buttons in nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,13 +5,13 @@ export default function Navbar() {
     return (
         <nav className="navbar sticky-top navbar-dark bg-dark">
             <h1 id="siteName">API-N-Dex</h1>
-            <button>
+            <button className="navButtons">
                 <Link to="/">Home</Link>
             </button>    
-            <button>
+            <button className="navButtons">
                 <Link to="/apis">API List</Link>
             </button>
-            <button>
+            <button className="navButtons">
                 <Link to="/new">Add A New API</Link>
             </button>
 
