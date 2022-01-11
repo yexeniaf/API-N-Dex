01a055687073fd6bff111bbc4823805ae51962a3
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-11 16:44:19 -0500

    nav bar margins

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,13 +6,13 @@ export default function Navbar() {
         <nav className="navbar sticky-top navbar-dark bg-dark justify-content-between">
             <h1 id="siteName" >API-N-Dex</h1>
             <div>
-             <button>
+             <button type="button" className="btn btn-warning">
                    <Link to="/">Home</Link>
              </button>    
-             <button>
+             <button type="button" className="btn btn-warning">
                  <Link to="/apis">API List</Link>
              </button>
-             <button>
+             <button type="button" className="btn btn-warning">
                     <Link to="/new">Add A New API</Link>
              </button>
             </div>
