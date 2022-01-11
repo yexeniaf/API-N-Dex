7ab708720aea350c9f9d762594734a336bba44b1
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-11 18:29:15 -0500

    rules in homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -3,12 +3,22 @@ import React from 'react'
 export default function HomePage() {
     return (
         <div class="jumbotron">
-            <h1 className="display-4">API-N-Dex</h1>
+            <h1 className="display-4" color='##E85A4F'>API-N-Dex</h1>
                 <p className="container lead">
                     Rules For This Site: 
                 </p>
                 <br/>
-                <ul className="rules container">
+                <ul className="rules-container">
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
+                    <br/>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
+                    <br/>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
+                    <br/>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
+                    <br/>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
+                    <br/>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
                 </ul>
         </div>
