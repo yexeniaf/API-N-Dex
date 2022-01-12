d5eb9520a1b9f4d118ee1738f1594ff4ae0c7a41
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-12 14:44:01 -0500

    removed jusfity content in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+            <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Home</Link>
