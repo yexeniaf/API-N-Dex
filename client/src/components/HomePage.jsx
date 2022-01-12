d49226d3a75ebbcc9a09a61a5a87a89d2b54c698
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-12 11:59:04 -0500

    change from p to div for rules

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -5,9 +5,10 @@ export default function HomePage() {
     return (
         <div class="jumbotron">
             <h1 className="display-4" style={{color:'salmon'}}>API-N-Dex</h1>
-                <p className="rules-container">
-                    Rules For This Site: 
-                <ul style={{listStyleType:'none'}}>
+                <div className="rules-container">
+                    <h2>Rules For This Site: </h2>
+                    <br/>
+                <ul id="rules" style={{listStyleType:'none'}}>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
                     <br/>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
@@ -20,7 +21,7 @@ export default function HomePage() {
                     <br/>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rem ut a quia aut, sunt commodi et minima neque fuga perferendis, omnis totam expedita odit sed eligendi doloremque facilis distinctio!</li>
                 </ul>
-                </p>
+                </div>
         </div>
         
     )
