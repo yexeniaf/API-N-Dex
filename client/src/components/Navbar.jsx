4a587d6489432d5cc762e01669d3ad8e8f2b120a
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-12 15:14:13 -0500

    search button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,23 +4,28 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-            <h1 id="siteName" >API-N-Dex</h1>
+            <h1 id="siteName">API-N-Dex</h1>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item">
+                    <li className="nav-item" style={{marginLeft: '100px'}}>
                         <Link className="nav-link" to="/">Home</Link>
                     </li>    
-                    <li className="nav-item">
+                    <li className="nav-item" style={{marginLeft: '70px'}}>
                         <Link className="nav-link" to="/apis">API List</Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" style={{marginLeft: '70px'}}>
                         <Link className="nav-link" to="/new">Add A New API</Link>
                     </li>
                 </ul>
+                <form className="search-form" style={{marginLeft: '430px'}}>
+                <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-light" type="submit" style={{marginLeft:'5px', color: 'salmon'}} >Search</button>
+            </form>
             </div>
+            
 
         </nav>
     )
