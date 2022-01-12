e6662425ee9e2580af956f0d474e1b814d14aa82
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-12 15:31:14 -0500

    update nav link font weight

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,17 +10,17 @@ export default function Navbar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item" style={{marginLeft: '100px'}}>
+                    <li className="nav-item" style={{marginLeft: '100px', fontWeight: '700'}}>
                         <Link className="nav-link" to="/">Home</Link>
                     </li>    
-                    <li className="nav-item" style={{marginLeft: '70px'}}>
+                    <li className="nav-item" style={{marginLeft: '70px', fontWeight: '700'}}>
                         <Link className="nav-link" to="/apis">API List</Link>
                     </li>
-                    <li className="nav-item" style={{marginLeft: '70px'}}>
+                    <li className="nav-item" style={{marginLeft: '70px', fontWeight: '700'}}>
                         <Link className="nav-link" to="/new">Add A New API</Link>
                     </li>
                 </ul>
-                <form className="search-form" style={{marginLeft: '430px'}}>
+                <form className="search-form" style={{marginLeft: '400px'}}>
                 <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
                 <button className="btn btn-light" type="submit" style={{marginLeft:'5px', color: 'salmon'}} >Search</button>
             </form>
