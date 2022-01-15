2728825b93734f72d122da5817bbfea93a8f3d59
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-01-14 19:51:23 -0500

    search form fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+          <div className="container-fluid">
             <Link to="/" style={{textDecoration: 'none'}}>
             <h1 id="siteName">API-N-Dex</h1>
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item" style={{marginLeft: '100px', fontWeight: '700'}}>
                         <Link className="nav-link" to="/">Home</Link>
                     </li>    
@@ -22,11 +23,12 @@ export default function Navbar() {
                         <Link className="nav-link" to="/new">Add A New API</Link>
                     </li>
                 </ul>
-                <form className="search-form" style={{marginLeft: '400px'}}>
-                <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button className="btn btn-light" type="submit" style={{marginLeft:'5px', color: 'salmon'}} >Search</button>
             </form>
             </div>
+          </div>
             
 
         </nav>
